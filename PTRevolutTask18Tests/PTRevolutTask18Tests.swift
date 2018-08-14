1656710b5e225e5114913ad89f07c96f2c7d955f
Petko Todorov <petko.todorov@infor.com>
2018-08-14 16:06:54 +0300

    Initial Commit

---
+++ PTRevolutTask18Tests/PTRevolutTask18Tests.swift
@@ -0,0 +1,36 @@
+//
+//  PTRevolutTask18Tests.swift
+//  PTRevolutTask18Tests
+//
+//  Created by Petko Todorov on 8/14/18.
+//  Copyright © 2018 Petko Todorov. All rights reserved.
+//
+
+import XCTest
+@testable import PTRevolutTask18
+
+class PTRevolutTask18Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
